026bf90417577bb98b55b145990990e6a7c17c33
mahadihasan2 <mahadihasan125643@gmail.com>
2022-03-21 13:08:08 +0600

    event-handler-with button

--- src/Components/Showbazar/Showbazar.js
+++ src/Components/Showbazar/Showbazar.js
@@ -1,12 +1,18 @@
 import React from 'react';
+import './Showbazar.css';
 
 const Showbazar = (props) => {
     const { name, price, id } = props.bazar
+    const addToCard = (id) => {
+        console.log('item-add..', id)
+    }
     return (
-        <div>
+        <div className='product'>
             <h2>Show the name:{name}</h2>
             <p>Show the price: ${price}</p>
-            <small>Show the id: {id}</small>
+            <small>Show the id: {id}</small><br>
+            </br>
+            <button onClick={() => addToCard(id)}>addToCard</button>
         </div>
     );
 };
